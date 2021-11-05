c1e819d4aa6c1179b59802a6948f2192adb0debf
yuaokuaishou <84902119+yuaokuaishou@users.noreply.github.com>
2021-11-05 15:52:34 +0800

    Update text_classification.py

--- text_classification.py
+++ text_classification.py
@@ -2,14 +2,7 @@ import fasttext
 
 
 def classifier(df, model_, threshold):
-    """
-    文本分类器
-    输出分类结果
-    :param text: 输入的告警文本
-    :param model_: 训练好的模型
-    :param threshold: 二分类判定概率阈值
-    :return: 分类结果
-    """
+
     pred_handle_status_ = []
     handle_probability_ = []
     model = fasttext.load_model(model_)  # 加载训练好的模型
