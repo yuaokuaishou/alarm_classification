720714a534c5193d9e40ba9aa524b1f201f9bc6c
yuaokuaishou <84902119+yuaokuaishou@users.noreply.github.com>
2021-11-05 15:39:31 +0800

    Add files via upload

---
+++ text_classification.py
@@ -0,0 +1,37 @@
+import fasttext
+
+
+def classifier(df, model_, threshold):
+    """
+    文本分类器
+    输出分类结果，1是接手，0是忽略
+    :param text: 输入的告警文本
+    :param model_: 训练好的模型
+    :param threshold: 二分类判定概率阈值
+    :return: 分类结果
+    """
+    pred_handle_status_ = []
+    handle_probability_ = []
+    model = fasttext.load_model(model_)  # 加载训练好的模型
+
+    for text in df['text_new']:
+        text = text.replace("\n", " ")  # 部分告警信息包换换行符"\n"
+        cancel_probability = model.predict(text)[1][0]  # result = [label, probability]
+        handle_probability = abs(1-cancel_probability)
+
+        # 根据阈值来判断是否会被接手
+        if handle_probability > threshold:
+            handle_status = 1
+        else:
+            handle_status = 0
+
+        handle_probability_.append(handle_probability)
+        pred_handle_status_.append(handle_status)
+
+    df['handle_status'] = pred_handle_status_
+    df['handle_probability'] = handle_probability_
+    label = df['label'].tolist()
+    handle_status = df['handle_status'].tolist()
+    print("alarm_num: ", sum(handle_status))
+    return df, label, handle_status
+
