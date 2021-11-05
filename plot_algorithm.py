c60ba1af255e788eba3c513081502d31dcd9d329
yuaokuaishou <84902119+yuaokuaishou@users.noreply.github.com>
2021-11-05 15:48:24 +0800

    Update plot_algorithm.py

--- plot_algorithm.py
+++ plot_algorithm.py
@@ -6,11 +6,7 @@ from tools import cut_week_to_days, chinese_font
 
 
 def plot_re_pre_day(data):
-    """
-    算法在本周中每一天的召回率和精确率
-    :param data: 原始告警文件，增加了预测结果，去除了传输数据
-    :return:
-    """
+
     # 把一周的数据分割为每一天
     data_day_1, data_day_2, data_day_3, data_day_4, data_day_5, data_day_6, data_day_7 = cut_week_to_days(data)
 
@@ -36,9 +32,9 @@ def plot_re_pre_day(data):
 
     # 画图
     # 召回率的点线图
-    plt.plot(week_days, recall_list[1:], color='orange', marker='o', label="Recall: 描述漏报情况")
+    plt.plot(week_days, recall_list[1:], color='orange', marker='o', label="Recall")
     # 精确率的点线图
-    plt.plot(week_days, precision_list[1:], color='dodgerblue', marker='^', label="Precision: 描述准确情况")
+    plt.plot(week_days, precision_list[1:], color='dodgerblue', marker='^', label="Precision")
     # 召回率的均值
     plt.hlines(recall_list[0], 'Monday', 'Sunday', colors="orange", linestyles="dashed")
     # 精确率的均值
@@ -46,7 +42,7 @@ def plot_re_pre_day(data):
     # 图例
     plt.legend()
     # 标题
-    plt.title('本周告警接手预测算法效果 \n 本周平均Recall: ' + str('%.2f%%' % recall_list[0]) + ' 本周平均Precision: ' + str('%.2f%%' % precision_list[0]))
+    plt.title('本周平均Recall: ' + str('%.2f%%' % recall_list[0]) + ' 本周平均Precision: ' + str('%.2f%%' % precision_list[0]))
 
     # 显示每个点的值
     for a, b in zip(week_days, recall_list[1:]):
@@ -61,7 +57,7 @@ def plot_re_pre_day(data):
     plt.grid(alpha=0.3)
     # 保存图片，清晰度
     plt.rcParams['savefig.dpi'] = 300
-    plt.savefig('/Users/yuao/PycharmProjects/smart-alarm-evaluation/Figures/algorithm_re_pre.png', bbox_inches='tight')
+    plt.savefig('algorithm_re_pre.png', bbox_inches='tight')
     plt.show()
     return
 
